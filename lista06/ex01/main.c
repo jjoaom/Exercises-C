e12c881782ccce9a6a377fe75192ae998217de46
jjoaom <joaomarcos.aquino@hotmail.com>
2023-10-16 12:43:48 -0300

    lista 06

---
+++ lista06/ex01/main.c
@@ -0,0 +1,16 @@
+/*1. Escreva um programa para preencher um vetor com 20 vinte valores inteiros (os valores podem
+ser lidos do teclado ou gerados automaticamente). Em seguida, o sistema deve solicitar ao
+usuário um valor, que deve ser pesquisado no vetor. Imprima as posições do vetor que armazena
+o valor informado.*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int vetor[20], x;
+
+printf("Digite um valor: ");
+scanf("%d", &x);
+
+
+return 0;
