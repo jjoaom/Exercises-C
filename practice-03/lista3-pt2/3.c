8f417d7f1515fea2c833c7134e1fe690f67ca4fe
jjoaom <joaomarcos.aquino@hotmail.com>
2024-09-09 19:49:39 -0300

    update

--- practice-03/lista3-pt2/3.c
+++ practice-03/lista3-pt2/3.c
@@ -15,6 +15,35 @@ Faixas etárias:
 
 int main(){
     setlocale(LC_ALL, "Portuguese");
+    int idades = 0,age = 0,j=0,k=0,l=0,m=0,n=0;
+    do
+    {
+        printf("Digite a quantidade de pessoas: ");
+        scanf("%d",&idades);
+    } while (idades <= 0);
+    
 
+    for (int i = 1; i <= idades; i++)
+    {
+            printf("Digite a idade da %dª pessoa: ",i);
+            scanf("%d",&age);
+        if(age >= 1 && age <= 15){
+            j++;
+        }else if(age >= 16 && age <= 30){
+            k++;
+        }else if(age >= 31 && age <= 45){
+            l++;
+        }else if(age >= 46 && age <= 60){
+            m++;
+        }else if(age >= 61){
+            n++;
+        }
+        
+    }
+    printf("Porcentagem de pessoas com idade entre 1 a 15 anos: %.2f%%\n",(float)j / idades * 100);
+    printf("Porcentagem de pessoas com idade entre 16 a 30 anos: %.2f%%\n",(float)k / idades * 100);
+    printf("Porcentagem de pessoas com idade entre 31 a 45 anos: %.2f%%\n",(float)l / idades * 100);
+    printf("Porcentagem de pessoas com idade entre 46 a 60 anos: %.2f%%\n",(float)m / idades * 100);
+    printf("Porcentagem de pessoas com idade com mais de 61 anos: %.2f%%\n",(float)n / idades * 100);
     return 0;
 }
--- practice-03/lista3-pt2/9.c
+++ practice-03/lista3-pt2/9.c
@@ -3,6 +3,9 @@
 mesmo tempo), por 2 e por 5. Caso algum número não seja divisível por nenhum desses números
 mostre a mensagem “Número não é divisível pelos valores”. Apresente também ao final a
 quantidade de números divisíveis por 3 e 9, por 2 e por 5
+
+laço for para digitar os 10 numeros e verificar
+preciso de um if para verificar as divisões, a questão é como dividir de forma funcional e eficiente.
 */
 
 #include <stdio.h>
@@ -10,6 +13,28 @@ quantidade de números divisíveis por 3 e 9, por 2 e por 5
 
 int main(){
     setlocale(LC_ALL, "Portuguese");
-
+    int num, j=0,k=0,l=0;
+    for (int i = 0; i < 10; i++)
+    {
+        printf("Digite o %dº número inteiro e  maior que zero:", i + 1);
+        scanf("%d", &num);
+        if (num % 3 == 0 && num % 9 == 0)
+        {
+            j++;
+            printf("O número %d é divisível por 3 e 9.\n",num);
+        }
+        if(num % 2 == 0){
+            k++;
+            printf("O número %d é divisivel por 2.\n",num);
+        }
+        if(num % 5 == 0){
+            l++;
+            printf("O númeor %d é divisivel por 5.\n",num);
+        }
+    }
+    printf("Quantidade de números divisíveis por 3 e 9 é %d\n",j);
+    printf("Quantidade de números divisíveis por 2 é %d\n",k);
+    printf("Quantidade de números divisíveis por 5 é %d\n",l);
+    
     return 0;
 }
